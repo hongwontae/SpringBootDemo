ce3fcfc3bb4ad8a1751486f5ed4e7dc00bd5f58f
hongwontae <feelchok1234@gmail.com>
2023-06-26 00:37:13 +0900

    Ss

--- b4/src/main/java/org/zerock/b4/controller/UpDownController.java
+++ b4/src/main/java/org/zerock/b4/controller/UpDownController.java
@@ -18,10 +18,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.zerock.b4.dto.RequestFileRemoveDTO;
 import org.zerock.b4.dto.UploadResultDTO;
 
 import lombok.extern.log4j.Log4j2;
