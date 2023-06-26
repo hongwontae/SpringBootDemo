9863838a878420fc6b7976917047b2f35ca7d91e
hongwontae <feelchok1234@gmail.com>
2023-06-26 23:05:30 +0900

    2023 06 26 security

--- b52/src/test/java/org/zerock/b52/B52ApplicationTests.java
+++ b52/src/test/java/org/zerock/b52/B52ApplicationTests.java
@@ -1,13 +1,34 @@
 package org.zerock.b52;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootTest
+@Log4j2
 class B52ApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Autowired
+    PasswordEncoder passwordEncoder;
+
+	   @Test
+    public void passwordEncode() throws Exception{
+        log.info((passwordEncoder.encode("1234")));
+    }
+
+	  @Test
+    public void passwordTest() throws Exception{
+    	String encodePasswd = "$2a$10$USbExG2YOZJqu5rR9eWAqO3NqwjS6c8uI0c695cnURA2gxqRnx41O";
+    	String password = "1234";
+    	boolean test = passwordEncoder.matches(password, encodePasswd);
+    	log.info(test);
+    }
 
 }
